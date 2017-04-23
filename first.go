99364a851da0204dc03bb7f55323260edc91593a
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-04-23 19:15:41 +0000

    basics outlined

--- first.go
+++ first.go
@@ -5,6 +5,8 @@ import (
         _ "github.com/boltdb/bolt"
 	"github.com/hashicorp/raft-boltdb"
 	"github.com/hashicorp/raft"
+
+
 )
 
 type Hack struct {
@@ -12,32 +14,6 @@ type Hack struct {
 }
 
 func main() {
-	// Open the my.db data file in your current directory.
-	// It will be created if it doesn't exist.
-	/*
-	db, err := bolt.Open("my.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	db.View(func(tx *bolt.Tx) error {
-			//b := tx.Bucket([]byte("Suffrage"))
-
-			c := tx.Cursor()
-			for k,v := c.First(); k != nil; k,v = c.Next() {
-			  log.Printf("key=%s, value=%s\n", k, v)
-			}
-
-			b := tx.Bucket([]byte("conf"))
-			c = b.Cursor()
-			for k,v := c.First(); k != nil; k,v = c.Next() {
-			  log.Printf("key=%s, value=%s\n", k, v)
-			}
-			return nil
-	})
-	log.Print("worked")
-	defer db.Close()
-*/
-	// Create the backend raft store for logs and stable storage.
 	store, err := raftboltdb.NewBoltStore("raft.db")
 	if err != nil {
 	    log.Fatal(err)
@@ -45,11 +21,11 @@ func main() {
 	if store != nil {
 	   log.Print("opened the bolt store");
 	}
-	//lastIndex, err := store.LastIndex
+	lastIndex, err := store.LastIndex()
+	log.Printf("%d",lastIndex)
+	raftLog := &raft.Log{}	
+	store.GetLog(lastIndex, raftLog)
+	log.Printf("%s", raftLog)
+	log.Printf("%s", raftLog.Data)
 	
-	raft, err := raft.NewRaft(nil, nil, store, store, nil, nil, nil)
-	if err != nil {
-	    log.Fatal(err)
-	}
-	if raft != nil { log.Print("got a raft")}
 }
