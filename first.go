5188cec82fd8838eb61b3a8b31224ecff6ab5bf9
sweeneyb <tab126@gmail.com>
2017-04-23 22:14:22 +0000

    begin forging a Log

--- first.go
+++ first.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"log"
-        _ "github.com/boltdb/bolt"
 	"github.com/hashicorp/raft-boltdb"
-	"github.com/hashicorp/raft"
+       	"github.com/hashicorp/raft"
 
 
 )
@@ -25,7 +24,15 @@ func main() {
 	log.Printf("%d",lastIndex)
 	raftLog := &raft.Log{}	
 	store.GetLog(lastIndex, raftLog)
-	log.Printf("%s", raftLog)
-	log.Printf("%s", raftLog.Data)
+	//log.Printf("%s", raftLog)
+	//log.Printf("%s", raftLog.Data)
+	var i uint64
+   	for i = 0; i<=lastIndex; i++ {
+	    store.GetLog(i, raftLog)
+	    log.Printf("%d", i)
+	    log.Printf("%s", raftLog)
+	}
+	var removeLog = Log{Index: i, Type: raft.LogRemovePeer, peer: "foo"}
+	log.Printf("%s", removeLog)
 	
 }
