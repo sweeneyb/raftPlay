c881feb7c696964db3f5280ba3e9bd28b9a7acb0
sweeneyb <tab126@gmail.com>
2017-04-24 01:05:08 +0000

    Added txn awareness; probably about to come out when I figure out how Logs are actually constructed

--- first.go
+++ first.go
@@ -22,17 +22,18 @@ func main() {
 	}
 	lastIndex, err := store.LastIndex()
 	log.Printf("%d",lastIndex)
-	raftLog := &raft.Log{}	
+	raftLog := &raft.Log{}
 	store.GetLog(lastIndex, raftLog)
 	//log.Printf("%s", raftLog)
 	//log.Printf("%s", raftLog.Data)
-	var i uint64
+	var i, term uint64
    	for i = 0; i<=lastIndex; i++ {
 	    store.GetLog(i, raftLog)
 	    log.Printf("%d", i)
 	    log.Printf("%s", raftLog)
 	}
-	var removeLog = Log{Index: i, Type: raft.LogRemovePeer, peer: "foo"}
+	term = raftLog.Term
+	var removeLog = Log{Index: i, Term: term, Type: raft.LogRemovePeer, peer: "foo"}
 	log.Printf("%s", removeLog)
 	
 }
