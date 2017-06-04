b6554cdc7ce5143defc1a7ae63d34f9e5b9e6f65
sweeneyb <tab126@gmail.com>
2017-06-04 04:12:30 +0000

    add reading from conf; no idea if it works since I don't know a key

--- conf.go
+++ conf.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"github.com/hashicorp/raft-boltdb"
 	"github.com/mitchellh/cli"
+	"log"
 )
 
 type ConfCommand struct {
 	Ui cli.Ui
 }
 
-func (c *ConfCommand) Run(_ []string) int {
+func (c *ConfCommand) Run(args []string) int {
 	c.Ui.Output("Conf run")
+	store, err := raftboltdb.NewBoltStore("raft.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if store != nil {
+		log.Print("opened the bolt store")
+	}
+	var key string = args[0]
+	c.Ui.Output(fmt.Sprintf("searching for %s", key))
+	message, err := store.Get([]byte(key))
+	if err != nil {
+		c.Ui.Output(fmt.Sprintf("%s", err))
+		return 1
+	}
+	c.Ui.Output(fmt.Sprintf("%s", message))
 	return 0
 }
 
